66f4cfef9b57816ffc1e08949879a0b0f952893b
Newton Ni <nwtnni@gmail.com>
2020-06-13 23:37:37 -0400

    Implement `Consts(t)`

--- chapter-03/src/term.rs
+++ chapter-03/src/term.rs
@@ -1,3 +1,7 @@
+use std::collections::HashSet;
+
+use maplit::hashset;
+
 /// The set of terms `T`.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum T<'a> {
@@ -63,4 +67,37 @@ pub enum T<'a> {
     /// if t₁ then t₂ else t₃ ∈ T
     /// ```
     IfElse(&'a T<'a>, &'a T<'a>, &'a T<'a>),
+}
+
+impl<'a> T<'a> {
+    /// Definition 3.3.1 - The set of constants `Consts(t)` appearing in a term `t`.
+    pub fn consts(&self) -> HashSet<T<'a>> {
+        use T::*;
+        match self {
+        // Consts(true) = { true }
+        | True => hashset!(True),
+
+        // Consts(false) = { false }
+        | False => hashset!(False),
+
+        // Consts(0) = { 0 }
+        | Zero => hashset!(Zero),
+
+        // Consts(succ t₁) = Consts(t₁)
+        | Succ(t_1) => t_1.consts(),
+
+        // Consts(pred t₁) = Consts(t₁)
+        | Pred(t_1) => t_1.consts(),
+
+        // Consts(iszero t₁) = Consts(t₁)
+        | IsZero(t_1) => t_1.consts(),
+
+        // Consts(if t₁ then t₂ else t₃) = Consts(t₁) ∪ Consts(t₂) ∪ Consts(t₃)
+        | IfElse(t_1, t_2, t_3) => t_1.consts()
+            .into_iter()
+            .chain(t_2.consts())
+            .chain(t_3.consts())
+            .collect(),
+        }
+    }
 }
