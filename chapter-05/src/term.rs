cdee16819b6100c3d8f603f1c178fee2d795e8f3
Newton Ni <nwtnni@gmail.com>
2020-06-16 12:00:23 -0400

    Extract free variables

--- chapter-05/src/term.rs
+++ chapter-05/src/term.rs
@@ -1,3 +1,7 @@
+use std::collections::HashSet;
+
+use maplit::hashset;
+
 /// The untyped lambda calculus.
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum T<'a> {
@@ -27,4 +31,28 @@ pub enum T<'a> {
     ///    t₁ t₂ ∈ T
     /// ```
     App(&'a T<'a>, &'a T<'a>),
+}
+
+impl<'a> T<'a> {
+    /// Definition 5.3.2 - The set of free variables of a term `t`, written FV(t).
+    pub fn free(&self) -> HashSet<String> {
+        use T::*;
+        match self {
+        // FV(x) = { x }
+        | Var(x) => hashset!(x.clone()),
+
+        // FV(λx. t₁) = FV(t₁) \ { x }
+        | Abs(x, t_1) => {
+            let mut fvs = t_1.free();
+            fvs.remove(x);
+            fvs
+        }
+
+        // FV(t₁, t₂) = FV(t₁) ∪ FV(t₂)
+        | App(t_1, t_2) => t_1.free()
+            .into_iter()
+            .chain(t_2.free())
+            .collect(),
+        }
+    }
 }
