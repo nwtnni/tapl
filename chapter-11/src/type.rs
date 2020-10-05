cbb5d68b46fe2e2ede13197f393c72dba7744240
Newton Ni <nwtnni@gmail.com>
2020-10-04 23:33:43 -0400

    Type check extensions

--- chapter-11/src/type.rs
+++ chapter-11/src/type.rs
@@ -9,6 +9,7 @@ use crate::term::Term;
 pub enum Type {
     Bool,
     Fun(Box<Type>, Box<Type>),
+    Tuple(Vec<Type>),
 }
 
 impl fmt::Display for Type {
@@ -16,6 +17,17 @@ impl fmt::Display for Type {
         match self {
         | Type::Bool => write!(fmt, "bool"),
         | Type::Fun(from, to) => write!(fmt, "{} -> {}", from, to),
+        | Type::Tuple(types) => {
+            let mut types = types.iter();
+            write!(fmt, "(")?;
+            if let Some(head) = types.next() {
+                write!(fmt, "{}", head)?;
+            }
+            while let Some(tail) = types.next() {
+                write!(fmt, ", {}", tail)?;
+            }
+            write!(fmt, ")")
+        }
         }
     }
 }
@@ -26,7 +38,16 @@ pub struct Context(HashMap<i64, Type>);
 impl<'a> Term<'a> {
     pub fn check(&self, context: &mut Context, depth: i64) -> anyhow::Result<Type> {
         match self {
-        | Term::Bool(_) => Ok(Type::Bool),
+        // ---------------- T-True
+        // Γ |- true : Bool
+        | Term::Bool(true)
+        // ----------------- T-False
+        // Γ |- false : Bool
+        | Term::Bool(false) => Ok(Type::Bool),
+
+        // Γ |- t₁ : Bool    Γ |- t₂ : T    Γ |- t₃ : T
+        // -------------------------------------------- T-If
+        //          if t₁ then t₂ else t₃ : T
         | Term::If { r#if, then, r#else } => {
             if r#if.check(context, depth)? != Type::Bool {
                 return Err(anyhow!("Guard of conditional not a boolean"));
@@ -38,23 +59,82 @@ impl<'a> Term<'a> {
             }
             Ok(then_type)
         }
-        | Term::Var { index } => context.0
+
+        // x : T ∈ Γ
+        // ---------- T-Var
+        // Γ |- x : T
+        | Term::Var(index) => context.0
             .get(index)
             .cloned()
             .ok_or_else(|| anyhow!("Unbound variable: {}", index)),
-        | Term::Abs { hint: _, r#type, term } => {
+
+        //    Γ, x : T₁ |- t₂ : T₂
+        // --------------------------- T-Abs
+        // Γ |- λx : T₁. t₂ : T₁ -> T₂
+        | Term::Abs { hint: _, r#type, body } => {
             context.0.insert(depth, r#type.clone());
-            let term_type = term.check(context, depth + 1)?;
+            let body_type = body.check(context, depth + 1)?;
             context.0.remove(&depth);
-            Ok(Type::Fun(Box::new(r#type.clone()), Box::new(term_type)))
+            Ok(Type::Fun(Box::new(r#type.clone()), Box::new(body_type)))
         }
+
+        // Γ |- t₁ : T₁₁ -> T₁₂    Γ |- t₂ : T₁₁
+        // ------------------------------------- T-App
+        //           Γ |- t₁ t₂ : T₁₂
         | Term::App { fun, arg } => {
             let fun_type = fun.check(context, depth)?;
             let arg_type = arg.check(context, depth)?;
             match fun_type {
             | Type::Fun(expected_arg_type, return_type) if *expected_arg_type == arg_type => Ok(*return_type),
             | Type::Fun(_, _) => Err(anyhow!("Parameter type mismatch")),
-            | Type::Bool => Err(anyhow!("Function type expected")),
+            | _ => Err(anyhow!("Function type expected")),
+            }
+        }
+
+        //   Γ |- t₁ : T
+        // ---------------- T-Ascribe
+        // Γ |- t₁ as T : T
+        | Term::Asc { term, r#type: expected_type } => {
+            let actual_type = term.check(context, depth)?;
+            if actual_type == *expected_type {
+                Ok(actual_type)
+            } else {
+                Err(anyhow!("Expected type {}, but found type {}", expected_type, actual_type))
+            }
+        }
+
+        // Γ |- t₁ : T₁    Γ, x : T₁ |- t₂ : T₂
+        // ------------------------------------ T-Let
+        //     Γ |- let x = t₁ in t₂ : T₂
+        | Term::Let { hint: _, arg, body } => {
+            let arg_type = arg.check(context, depth)?;
+            context.0.insert(depth, arg_type);
+            let body_type = body.check(context, depth + 1)?;
+            context.0.remove(&depth);
+            Ok(body_type)
+        }
+
+        //         for each i, Γ |- t_i : T_i
+        // ---------------------------------------- T-Tuple
+        // Γ |- (t_i^{i ∈ 0..n}) : (T_i^{i ∈ 0..n})
+        | Term::Tuple(terms) => {
+            terms
+                .iter()
+                .map(|term| term.check(context, depth))
+                .collect::<Result<Vec<_>, _>>()
+                .map(Type::Tuple)
+        }
+
+        // Γ |- t₁ : (T_i^{i ∈ 0..n})
+        // -------------------------- T-Proj
+        //      Γ |- t₁.j : T_j
+        | Term::Project { tuple, index } => {
+            match tuple.check(context, depth)? {
+            | Type::Tuple(mut types) if *index < types.len() => {
+                Ok(types.swap_remove(*index))
+            }
+            | Type::Tuple(_) => Err(anyhow!("Projecting more elements than tuple contains")),
+            | _ => Err(anyhow!("Can only project tuples")),
             }
         }
         }
